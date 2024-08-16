7806f345aab0ed71edc8632706bd43e646744954
ziadhaggagNZ <ziadhaggagNZ@gmail.com>
2024-08-16 21:58:35 +0300

    Container & Text

--- 02_flutter_basics/lib/main.dart
+++ 02_flutter_basics/lib/main.dart
@@ -10,15 +10,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text("Fluttar Basics"),),
-        body: const Center(
-          child: Text("Hello world!"),
+        home: Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Fluttar Basics"),
+      ),
+      body:  Center(
+        child:  Container(
+          height: 200,
+          width: 200,
+          decoration: BoxDecoration(color: Colors.blue,borderRadius: BorderRadius.circular(20)),
+          child:  Center(
+            child: Text(
+              "Hello world!",
+              style: TextStyle(color: Colors.red, fontSize: 20),
+            ),
+          ),
         ),
-      )
-    );
+      ),
+    ));
   }
 }
-
