9de24e9bc227509c6c48707c94db186970199d39
ziadhaggagNZ <ziadhaggagNZ@gmail.com>
2024-08-16 20:46:02 +0300

    frist commit

--- 02_flutter_basics/lib/main.dart
+++ 02_flutter_basics/lib/main.dart
@@ -11,7 +11,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text("Fluttar Basics"),),
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text("Fluttar Basics"),),
         body: const Center(
           child: Text("Hello world!"),
         ),
