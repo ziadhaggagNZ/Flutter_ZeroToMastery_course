41bd581bb8031eb8b144ff0cc5a27d8ce842ac4e
ziadhaggagNZ <ziadhaggagNZ@gmail.com>
2024-08-17 23:49:36 +0300

    ScrollView

--- 02_flutter_basics/lib/main.dart
+++ 02_flutter_basics/lib/main.dart
@@ -20,52 +20,57 @@ class MyApp extends StatelessWidget {
         decoration: BoxDecoration(
           //color: Colors.blue,
         borderRadius: BorderRadius.circular(20)),
-        child:  Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            const SizedBox(
-              child: Text("data",style: TextStyle( fontSize: 20),),
-            ),
-             Container(
-              //color: Colors.red,
-              child: const Text(
-                "Hello world!",
-                style: TextStyle( fontSize: 20),
+        child:  SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            //mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const SizedBox(
+                child: Text("data",style: TextStyle( fontSize: 20),),
               ),
-            ),
-            // Container(
-            //   // padding: EdgeInsets.all(5),
-            //   // margin: EdgeInsets.all(5),
-            //   //color: Colors.green,
-            //   child: const Expanded(
-            //     child: Text(
-            //     "Hello world! my name is ziad haggag",
-            //     maxLines: 1,
-            //     overflow: TextOverflow.ellipsis,
-            //     style: TextStyle( fontSize: 20),
-            //   ),),
-            //),
-            Stack(
-              children: [
-                SizedBox(
-                height: 200,
-                width: 200,
-                child: Image.asset("assets/ziad.png",fit: BoxFit.cover,)
+               Container(
+                //color: Colors.red,
+                child: const Text(
+                  "Hello world!",
+                  style: TextStyle( fontSize: 20),
                 ),
-                Positioned(
-                  top: 30,
-                  left: 50,
-                  child: Text("ziad haggag",style: TextStyle(color: Colors.red,fontSize: 20),)
-                  )
-              ]
-            ),
-              SizedBox(
-                height: 100,
-                width: 100,
-                child: CircleAvatar(backgroundImage: AssetImage("assets/ziad.png"),),
-              )
-          ],
+              ),
+              // Container(
+              //   // padding: EdgeInsets.all(5),
+              //   // margin: EdgeInsets.all(5),
+              //   //color: Colors.green,
+              //   child: const Expanded(
+              //     child: Text(
+              //     "Hello world! my name is ziad haggag",
+              //     maxLines: 1,
+              //     overflow: TextOverflow.ellipsis,
+              //     style: TextStyle( fontSize: 20),
+              //   ),),
+              //),
+              SizedBox(height: 200,),
+              Stack(
+                children: [
+                  SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: Image.asset("assets/ziad.png",fit: BoxFit.cover,)
+                  ),
+                  Positioned(
+                    top: 30,
+                    left: 50,
+                    child: Text("ziad haggag",style: TextStyle(color: Colors.red,fontSize: 20),)
+                    )
+                ]
+              ),
+                          SizedBox(height: 200,),
+          
+                SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: CircleAvatar(backgroundImage: AssetImage("assets/ziad.png"),),
+                )
+            ],
+          ),
         )
       ),
     ));
