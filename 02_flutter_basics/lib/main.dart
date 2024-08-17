70cd9c6384e065374961a9e2b205c402a7f86cb0
ziadhaggagNZ <ziadhaggagNZ@gmail.com>
2024-08-18 00:05:00 +0300

    ScrollVie

--- 02_flutter_basics/lib/main.dart
+++ 02_flutter_basics/lib/main.dart
@@ -15,64 +15,81 @@ class MyApp extends StatelessWidget {
         centerTitle: true,
         title: const Text("Fluttar Basics"),
       ),
-      body:  Container(
-
-        decoration: BoxDecoration(
-          //color: Colors.blue,
-        borderRadius: BorderRadius.circular(20)),
-        child:  SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            //mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              const SizedBox(
-                child: Text("data",style: TextStyle( fontSize: 20),),
-              ),
-               Container(
-                //color: Colors.red,
-                child: const Text(
-                  "Hello world!",
-                  style: TextStyle( fontSize: 20),
-                ),
-              ),
-              // Container(
-              //   // padding: EdgeInsets.all(5),
-              //   // margin: EdgeInsets.all(5),
-              //   //color: Colors.green,
-              //   child: const Expanded(
-              //     child: Text(
-              //     "Hello world! my name is ziad haggag",
-              //     maxLines: 1,
-              //     overflow: TextOverflow.ellipsis,
-              //     style: TextStyle( fontSize: 20),
-              //   ),),
-              //),
-              SizedBox(height: 200,),
-              Stack(
-                children: [
-                  SizedBox(
-                  height: 200,
-                  width: 200,
-                  child: Image.asset("assets/ziad.png",fit: BoxFit.cover,)
+      body: Container(
+          decoration: BoxDecoration(
+              //color: Colors.blue,
+              borderRadius: BorderRadius.circular(20)),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              //mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const SizedBox(
+                  child: Text(
+                    "data",
+                    style: TextStyle(fontSize: 20),
                   ),
+                ),
+                Container(
+                  //color: Colors.red,
+                  child: const Text(
+                    "Hello world!",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                // Container(
+                //   // padding: EdgeInsets.all(5),
+                //   // margin: EdgeInsets.all(5),
+                //   //color: Colors.green,
+                //   child: const Expanded(
+                //     child: Text(
+                //     "Hello world! my name is ziad haggag",
+                //     maxLines: 1,
+                //     overflow: TextOverflow.ellipsis,
+                //     style: TextStyle( fontSize: 20),
+                //   ),),
+                //),
+                SizedBox(
+                  height: 200,
+                ),
+                Stack(children: [
+                  SizedBox(
+                      height: 200,
+                      width: 200,
+                      child: Image.asset(
+                        "assets/ziad.png",
+                        fit: BoxFit.cover,
+                      )),
                   Positioned(
-                    top: 30,
-                    left: 50,
-                    child: Text("ziad haggag",style: TextStyle(color: Colors.red,fontSize: 20),)
-                    )
-                ]
-              ),
-                          SizedBox(height: 200,),
-          
+                      top: 30,
+                      left: 50,
+                      child: Text(
+                        "ziad haggag",
+                        style: TextStyle(color: Colors.red, fontSize: 20),
+                      ))
+                ]),
+                SizedBox(
+                  height: 100,
+                ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    color: Colors.green,
+                  child: Center(child: Text("data")),
+                ),
+                SizedBox(
+                  height: 200,
+                ),
+              
                 SizedBox(
                   height: 100,
                   width: 100,
-                  child: CircleAvatar(backgroundImage: AssetImage("assets/ziad.png"),),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage("assets/ziad.png"),
+                  ),
                 )
-            ],
-          ),
-        )
-      ),
+              ],
+            ),
+          )),
     ));
   }
 }
