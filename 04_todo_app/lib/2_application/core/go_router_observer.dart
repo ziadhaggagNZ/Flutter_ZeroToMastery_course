e602280d8bea3639ec9b87ba5ad416a285a01788
ziadhaggagNZ <ziadhaggagNZ@gmail.com>
2024-08-26 19:06:51 +0300

    go router

---
+++ 04_todo_app/lib/2_application/core/go_router_observer.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class GoRouterObserver extends NavigatorObserver {
+  GoRouterObserver();
+  
+  @override
+  void didPop(Route route, Route? previousRoute) {
+    debugPrint('didPush : ${route.settings.name}');
+  }
+
+  @override
+  void didPush(Route route, Route? previousRoute) {
+    debugPrint('didPush : ${route.settings.name} previousRoute: ${previousRoute?.settings.name}');
+  }
+
+  @override
+  void didRemove(Route route, Route? previousRoute) {
+    debugPrint('didRemove : ${route.settings.name} previousRoute: ${previousRoute?.settings.name}');
+  }
+
+  @override
+  void didReplace({Route? newRoute, Route? oldRoute}) {
+    debugPrint('didReplace : ${newRoute?.settings.name} oldRoute: ${oldRoute?.settings.name}');
+  }
+
+  @override
+  void didStartUserGesture(Route route, Route? previousRoute) {
+    debugPrint('didStartUserGesture : ${route.settings.name} previousRoute: ${previousRoute?.settings.name}');
+  }
+
+  @override
+  void didStopUserGesture() {
+    debugPrint('didStopUserGesture');
+  }
+
+}
